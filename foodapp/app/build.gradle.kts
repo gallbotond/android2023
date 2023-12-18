1661d085258127cbb2cf17d5d4de54f7e7c9efc1
Gáll Botond <gallbotondofficial@gmail.com>
2023-12-18 13:54:03 +0200

    Revert "asd"
    This reverts commit f24e01a9054ca6a1f520bc384623064a21247b47.

--- foodapp/app/build.gradle.kts
+++ foodapp/app/build.gradle.kts
@@ -70,8 +70,4 @@ dependencies {
 
     // Jetpack Compose Integration
     implementation("androidx.navigation:navigation-compose:$nav_version")
-
-    // Room dependencies
-    implementation("androidx.room:room-runtime:2.6.0")
-    implementation("androidx.room:room-ktx:2.6.0")
 }
